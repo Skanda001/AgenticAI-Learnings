139f6645cb62ae7aec8a08b1ca75aeaa9cdab0a7
Skanda <bsskanda@gmail.comm>
2026-09-11 10:26:16 +0530

    LAst

--- tools.py
+++ tools.py
@@ -4,8 +4,7 @@ from bs4 import BeautifulSoup
 from tavily import TavilyClient
 import os
 from dotenv import load_dotenv
-from rich import print
-load_dotenv
+load_dotenv()
 
 tavily = TavilyClient(api_key = os.getenv("TAVILY_API_KEY"))
 
