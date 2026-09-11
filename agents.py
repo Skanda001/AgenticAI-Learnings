081750e411044b207a12fc171b3c9021318d2c16
Skanda <bsskanda@gmail.comm>
2026-09-11 23:00:34 +0530

    Change

--- agents.py
+++ agents.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 llm = ChatOpenAI(
-    model="meta-llama/llama-3.3-70b-instruct:free",
+    model="openrouter/free",
     temperature=0,
     base_url="https://openrouter.ai/api/v1",
     api_key=os.getenv("OPENROUTER_API_KEY"),
